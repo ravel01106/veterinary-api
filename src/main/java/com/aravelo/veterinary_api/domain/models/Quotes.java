1e0826b528e5804dc2ab36b97968c85cf3d97dae
ravel01106 <amandaravelocabrera@gmail.com>
2024-08-12 12:46:45 +0100

    feat: created Quotes entity

---
+++ src/main/java/com/aravelo/veterinary_api/domain/models/Quotes.java
@@ -0,0 +1,70 @@
+package com.aravelo.veterinary_api.domain.models;
+
+public class Quotes {
+  String id;
+  String petName;
+  String ownerName;
+  String date;
+  String time;
+  String symptoms;
+
+  public Quotes(String petName, String ownerName, String date, String time, String symptoms) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.date = date;
+    this.time = time;
+    this.symptoms = symptoms;
+  }
+
+  public Quotes() {
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getPetName() {
+    return petName;
+  }
+
+  public void setPetName(String petName) {
+    this.petName = petName;
+  }
+
+  public String getOwnerName() {
+    return ownerName;
+  }
+
+  public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public String getSymptoms() {
+    return symptoms;
+  }
+
+  public void setSymptoms(String symptoms) {
+    this.symptoms = symptoms;
+  }
+
+}
