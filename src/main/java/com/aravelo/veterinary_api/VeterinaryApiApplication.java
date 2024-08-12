6a2770d1915d363b500e4533cb5d50b10614d363
ravel01106 <amandaravelocabrera@gmail.com>
2024-08-12 12:34:40 +0100

    first commit

---
+++ src/main/java/com/aravelo/veterinary_api/VeterinaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.aravelo.veterinary_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aravelo/veterinary_api/VeterinaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aravelo.veterinary_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
