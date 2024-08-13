f5068e0939471f5deada4e716e493e402207f554
ravel01106 <amandaravelocabrera@gmail.com>
2024-08-13 08:10:12 +0100

    feat: added Repository annotations in the repositories

--- src/main/java/com/aravelo/veterinary_api/domain/repositories/QuoteRepository.java
+++ src/main/java/com/aravelo/veterinary_api/domain/repositories/QuoteRepository.java
@@ -1,9 +1,11 @@
 package com.aravelo.veterinary_api.domain.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.aravelo.veterinary_api.domain.models.Quote;
 
+@Repository
 public interface QuoteRepository extends CrudRepository<Quote, Long>{
 
 }
