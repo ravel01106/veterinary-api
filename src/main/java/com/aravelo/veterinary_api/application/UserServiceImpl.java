3af57a455905e90e18df094143647aae6b85ca51
ravel01106 <ravello2002@gmail.com>
2024-08-18 23:04:34 +0100

    Test(Green): give us the User if it exists

--- src/main/java/com/aravelo/veterinary_api/application/UserServiceImpl.java
+++ src/main/java/com/aravelo/veterinary_api/application/UserServiceImpl.java
@@ -1,13 +1,26 @@
 package com.aravelo.veterinary_api.application;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.aravelo.veterinary_api.domain.models.User;
+import com.aravelo.veterinary_api.domain.repositories.UserRepository;
 import com.aravelo.veterinary_api.domain.services.UserService;
 
 public class UserServiceImpl implements UserService{
 
+  @Autowired
+  UserRepository userRepository;
+
   @Override
   public User getUserByName(String name) {
-    return new User("","");
+    User user = null;
+    Optional<User> userOptional = userRepository.findByUsername(name);
+    if (userOptional.isPresent()) {
+      user = userOptional.get();
+    }
+    return user;
   }
 
 }
