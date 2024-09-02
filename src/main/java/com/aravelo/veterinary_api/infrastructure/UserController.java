2eca54f6c175e27311b62781b6a981ba196e768b
ravel01106 <amandaravelocabrera@gmail.com>
2024-09-02 12:58:59 +0100

    Test(Green): log in an existing user

--- src/main/java/com/aravelo/veterinary_api/infrastructure/UserController.java
+++ src/main/java/com/aravelo/veterinary_api/infrastructure/UserController.java
@@ -1,7 +1,29 @@
 package com.aravelo.veterinary_api.infrastructure;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aravelo.veterinary_api.domain.models.User;
+import com.aravelo.veterinary_api.domain.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
 public class UserController {
 
+  @Autowired
+  private UserService userService;
 
+  @PostMapping("/login")
+  public ResponseEntity<User> loginUser(@RequestBody User user){
+
+    User userdb = userService.getUserByName(user.getUsername());
+
+    return new ResponseEntity<User>(userdb, HttpStatus.OK);
+
+  }
 
 }
