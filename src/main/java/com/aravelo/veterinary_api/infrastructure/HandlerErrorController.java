d9f559f6f91eb1254962b4e5825f48f3255fb707
ravel01106 <ravello2002@gmail.com>
2024-09-08 19:51:11 +0100

    Test(Green): throw error when access the wrong path

--- src/main/java/com/aravelo/veterinary_api/infrastructure/HandlerErrorController.java
+++ src/main/java/com/aravelo/veterinary_api/infrastructure/HandlerErrorController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.aravelo.veterinary_api.domain.enums.ErrorTypes;
 import com.aravelo.veterinary_api.domain.error.ErrorMessage;
@@ -15,6 +16,7 @@ import com.aravelo.veterinary_api.domain.error.UserNotFoundException;
 @ControllerAdvice
 public class HandlerErrorController {
 
+
   @ExceptionHandler(UserNotFoundException.class)
   public ResponseEntity<ErrorMessage> notFoundUser(UserNotFoundException ex){
     ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), ErrorTypes.USER_NOT_EXISTS);
@@ -22,11 +24,13 @@ public class HandlerErrorController {
 
   }
 
+
   @ExceptionHandler(QuoteNotFoundException.class)
   public ResponseEntity<ErrorMessage> notFoundQuote(QuoteNotFoundException ex){
     ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), ErrorTypes.QUOTE_NOT_FOUND);
     return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
   }
+
 
   @ExceptionHandler(QuoteIsAlreadyExistException.class)
   public ResponseEntity<ErrorMessage> thereIsQuoteWithSameDate(QuoteIsAlreadyExistException ex){
@@ -35,10 +39,17 @@ public class HandlerErrorController {
 
   }
 
+
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<ErrorMessage> isEmptyBody(HttpMessageNotReadableException ex){
     ErrorMessage errorMessage = new ErrorMessage("The body is empty!!", ErrorTypes.EMPTY_BODY);
     return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NoResourceFoundException.class)
+  public ResponseEntity<ErrorMessage> getIncorrectPath(NoResourceFoundException ex){
+    ErrorMessage errorMessage = new ErrorMessage("This path does not exist !!!", ErrorTypes.FAIL_PATH);
+    return new ResponseEntity<ErrorMessage>(errorMessage, HttpStatus.NOT_FOUND);
   }
 
 }
